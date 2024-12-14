37f03d24a93daf2a08b8e7e6fafdb8565444e1a2
Giancarlo <giancarlo-misasi@users.noreply.github.com>
2024-12-14 00:20:23 -0800

    bugfix: create new session if none exist

--- setup_profile.sh
+++ setup_profile.sh
@@ -1,5 +1,9 @@
 echo 'eval "$(~/.local/bin/mise activate zsh)"' >> ~/.zshrc
 echo 'git config --global core.editor "nvim"' >> ~/.zshrc
 if command -v tmux &> /dev/null && [ -z "$TMUX" ]; then
-    tmux a # attach to the last session
+  if ! tmux has-session 2>/dev/null; then
+    tmux new-session -s default
+  else
+    tmux a
+  fi
 fi
